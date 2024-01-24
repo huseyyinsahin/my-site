be99e9fbc7b021434064525d8ea56ae669af0644
huseyyinsahin <huseysahin19@outlook.com>
2024-01-24 18:01:54 +0300

    Site update

--- script.js
+++ script.js
@@ -54,3 +54,22 @@ function scrollFunction() {
     document.documentElement.scrollTop > 100
   );
 }
+
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      // Burada form başarıyla gönderildikten sonra yapılacak işlemleri ekleyebilirsiniz.
+      alert("Form submitted successfully!");
+    })
+    .catch((error) => alert(error));
+};
